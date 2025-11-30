c0216e99453ad53fe92a3adfa312ffa629dc8beb
kaito <kaitoo.dev@gmail.com>
2025-12-01 01:12:53 +0900

    Update: home.nixでシェルの設定ファイルのシンボリックリンクを自動作成できるように

--- home.nix
+++ home.nix
@@ -1,18 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "kaito";
-  home.homeDirectory = "/Users/kaito";
+  home.homeDirectory = "/Users/${config.home.username}";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+#   This value determines the Home Manager release that your configuration is
+#   compatible with. This helps avoid breakage when a new Home Manager release
+#   introduces backwards incompatible changes.
+  
+#   You should not change this value, even if you update Home Manager. If you do
+#   want to update the value, then make sure to first check the Home Manager
+#   release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
@@ -49,6 +47,24 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+
+
+	# bash
+	".bash_profile".source = ./shell/bash/.bash_profile;
+	".bashrc".source = ./shell/bash/.bashrc;
+
+	# zsh
+	".zshrc".source = ./shell/zsh/.zshrc;
+	".zprofile".source = ./shell/zsh/.zprofile;
+
+	# fish
+	".config/fish" = {
+      source = ./shell/fish;
+      recursive = true;
+    };
+
+	# starship
+	".config/starship.toml".source = ./shell/starship.toml;
   };
 
   # Home Manager can also manage your environment variables through
